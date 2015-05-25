17a2b5cff6c274afb0a42282558b417cb0c2aaab
JoeBew42 <joebew42@gmail.com>
2015-05-25 23:11:29 +0200

    Making FormatterFactory a collaborator of TennigGame1

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -5,10 +5,16 @@ public class TennisGame1 implements TennisGame {
     private int m_score2 = 0;
     private String player1Name;
     private String player2Name;
+    private FormatterFactory formatterFactory;
 
     public TennisGame1(String player1Name, String player2Name) {
+        this(player1Name, player2Name, new FormatterFactory());
+    }
+
+    public TennisGame1(String player1Name, String player2Name, FormatterFactory formatterFactory) {
         this.player1Name = player1Name;
         this.player2Name = player2Name;
+        this.formatterFactory = formatterFactory;
     }
 
     public void wonPoint(String playerName) {
@@ -27,7 +33,6 @@ public class TennisGame1 implements TennisGame {
         int score1 = m_score1;
         int score2 = m_score2;
 
-        FormatterFactory formatterFactory = new FormatterFactory();
         Formatter formatter = formatterFactory.buildFor(score1, score2);
 
         return formatter.format(score1, score2);
